8430353b374e14c8d39a2b197666cb217bcfe221
1wj <W--j--@outlook.com>
2022-05-23 17:28:02 +0800

    23号测试fastgihub

---
+++ ch04crm/src/main/java/com/bjpowernode/crm/vo/testVo.java
@@ -0,0 +1,14 @@
+package com.bjpowernode.crm.vo;
+
+/**
+ * @file: ch04crm
+ * @version: 2021.1
+ * @Description: TODO
+ * @Author: Wj
+ * @Date: 2022/5/23 17:22
+ */
+public class testVo {
+        public  static void tw(){
+            System.out.println("ceshi");
+        }
+}
